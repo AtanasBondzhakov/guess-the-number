d7be0aba692d9f8334ffdec5c1c98c17313f29ec
Atanas Bondzhakov <atanas.b@yahoo.com>
2026-04-16 23:19:48 +0300

    Add styles and pressHandler to PrimaryButton

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function PrimaryButton({children}) {
+export default function PrimaryButton({ children }) {
+    const pressHandler = () => {
+        console.log('Pressed');
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                onPress={pressHandler}
+                style={({ pressed }) => (
+                    pressed ?
+                        [styles.buttonInnerContainer, styles.pressed] :
+                        styles.buttonInnerContainer
+                )}
+                android_ripple={{color: '#640233'}}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        margin: 4,
+        shadowColor: '#000',
+        shadowOpacity: 0.25,
+        shadowRadius: 2,
+        shadowOffset: { width: 0, height: 2 },
+    },
+    buttonInnerContainer: {
+        borderRadius: 28,
+        backgroundColor: '#92074d',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+        overflow: 'hidden'
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
+    }
+});
