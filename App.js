31d2cdaaed476833735e3c6dd956726982968669
Atanas Bondzhakov <atanas.b@yahoo.com>
2026-04-21 23:03:11 +0300

    Add GameOverScreen and logic to show it

--- App.js
+++ App.js
@@ -6,39 +6,51 @@ import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen.js';
 import GameScreen from './screens/GameScreen.js';
 import Colors from './constants/colors.js';
+import GameOverScreen from './screens/GameOverScreen.js';
 
 export default function App() {
-	const [userNumber, setUserNumber] = useState(null);
+  const [userNumber, setUserNumber] = useState(null);
+  const [gameIsOver, setGameIsOver] = useState(false);
 
-	const pickedNumberHandler = (pickedNumber) => {
-		setUserNumber(pickedNumber);
-	}
+  const pickedNumberHandler = (pickedNumber) => {
+    setUserNumber(pickedNumber);
+  }
 
-	return (
-		<SafeAreaProvider>
-			<LinearGradient
-				style={styles.rootContainer}
-				colors={[Colors.primary700, Colors.accent500]}
-			>
-				<ImageBackground
-					source={require('./assets/images/background.png')}
-					resizeMode='cover'
-					style={styles.rootContainer}
-					imageStyle={styles.backgroundImage}
-				>
-					{userNumber && <GameScreen userNumber={userNumber} />}
-					{!userNumber && <StartGameScreen onPickNumber={pickedNumberHandler} />}
-				</ImageBackground>
-			</LinearGradient>
-		</SafeAreaProvider>
-	);
+  const gameOverHandler = () => {
+    setGameIsOver(true);
+  }
+
+  return (
+    <SafeAreaProvider>
+      <LinearGradient
+        style={styles.rootContainer}
+        colors={[Colors.primary700, Colors.accent500]}
+      >
+        <ImageBackground
+          source={require('./assets/images/background.png')}
+          resizeMode='cover'
+          style={styles.rootContainer}
+          imageStyle={styles.backgroundImage}
+        >
+          {(userNumber && !gameIsOver) &&
+            <GameScreen
+              userNumber={userNumber}
+              onGameOver={gameOverHandler}
+            />
+          }
+          {!userNumber && <StartGameScreen onPickNumber={pickedNumberHandler} />}
+          {(userNumber && gameIsOver) && <GameOverScreen />}
+        </ImageBackground>
+      </LinearGradient>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-	rootContainer: {
-		flex: 1
-	},
-	backgroundImage: {
-		opacity: 0.15
-	}
+  rootContainer: {
+    flex: 1
+  },
+  backgroundImage: {
+    opacity: 0.15
+  }
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Alert, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -19,9 +19,15 @@ const generateRandomBetween = (min, max, exclude) => {
 let minBoundary = 1;
 let maxBoundary = 100;
 
-export default function GameScreen({ userNumber }) {
+export default function GameScreen({ userNumber, onGameOver }) {
     const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber)
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    useEffect(() => {
+        if (userNumber === currentGuess) {
+            onGameOver();
+        }
+    }, [userNumber, currentGuess]);
 
     const nextGuessHandler = (direction) => {
         if (
